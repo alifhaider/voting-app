79a098ec27456eae2ab7c640620ec9ac3533ab5d
itsAlif <macbookpro@MacBooks-MacBook-Pro.local>
2022-07-17 04:01:35 +0600

    Initialized app with tailwind

---
+++ app/root.tsx
@@ -0,0 +1,39 @@
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import styles from "./styles/app.css";
+
+export let meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "Voting App",
+  description: "Vote for your favorite soft-drink",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export let links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: styles }];
+};
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+        <h1 className="text-3xl text-lame-300">Hello, cruel world! </h1>
+      </body>
+    </html>
+  );
+}
